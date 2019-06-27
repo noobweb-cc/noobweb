7b9bb10780de266f6ae0229ec3505753477b8d38
hongfei.tian <hongfei.tian@zhangmen.com>
2019-06-27 20:02:53 +0800

    nw-button

---
+++ src/packages/button/src/main.vue
@@ -0,0 +1,25 @@
+<template>
+    <main>
+        
+    </main>
+</template>
+<script>
+export default {
+    name: 'nw-button',
+    props: {
+        type: {
+            type: String,
+            defulte: 'danger'
+        }
+    },
+    data () {
+        return {
+            
+        }
+    }
+}
+</script>
+<style lang="less" scoped>
+
+</style>
+
